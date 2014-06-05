8823fa337d054794af375a0ffdc09717b34f05a0
Niclas Hüppmeier <niclas_hueppmeier@gmx.de>
2014-06-05 08:38:03 +0200

    Unity3D Ordner mit HauptSpiel

---
+++ Unity3D_Projekte/Quantums_Quest_HauptSpiel/Assets/KartenMenue/GameControll_KartenMenue.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControll_KartenMenue : MonoBehaviour {
+
+    [HideInInspector]
+    private int screenWidth;
+    private int screenHeight;
+    private double scaleWidth;
+    private double scaleHeight;
+	// Use this for initialization
+	void Start () {
+        screenHeight = Screen.height;
+        screenWidth = Screen.width;
+
+        scaleWidth = screenWidth / 100;
+        scaleHeight = screenHeight / 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect((float)((screenWidth) - ((50 * scaleWidth))), (float)((screenHeight) - (5 * scaleHeight) * 2), (float)(50 * scaleWidth), (float)(5 * scaleHeight)), "QR-Code Scannen"))
+        {
+            Application.LoadLevel(6);
+        }
+    }
+}
---
+++ Unity3D_Projekte/Quantums_Quest_HauptSpiel/Assets/LogIn/GameControll_LogIn.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControll_LogIn : MonoBehaviour {
+
+    [HideInInspector]
+    private int screenWidth;
+    private int screenHeight;
+    private double scaleWidth;
+    private double scaleHeight;
+	// Use this for initialization
+	void Start () {
+        screenHeight = Screen.height;
+        screenWidth = Screen.width;
+
+        scaleWidth = screenWidth / 100;
+        scaleHeight = screenHeight / 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.TextField(new Rect((float)((screenWidth / 2) - ((50 * scaleWidth) / 2)), (float)((screenHeight / 2) - (5 * scaleHeight)), (float)(50 * scaleWidth), (float)(5 * scaleHeight)), "UserName");
+        GUI.TextField(new Rect((float)((screenWidth / 2) - ((50 * scaleWidth) / 2)), (float)((screenHeight / 2) + (5 * scaleHeight)), (float)(50 * scaleWidth), (float)(5 * scaleHeight)), "PassWord");
+
+        if (GUI.Button(new Rect((float)((screenWidth / 2) - ((50 * scaleWidth) / 2)), (float)((screenHeight / 2) + (5 * scaleHeight) * 3), (float)(50 * scaleWidth), (float)(5 * scaleHeight)), "LogIn"))
+        {
+            Screen.orientation = ScreenOrientation.Landscape;
+            Application.LoadLevel(1);
+        }
+
+        if (GUI.Button(new Rect((float)((screenWidth / 2) - ((50 * scaleWidth) / 2)), (float)((screenHeight / 2) + (5 * scaleHeight) * 5), (float)(50 * scaleWidth), (float)(5 * scaleHeight)), "Registrieren"))
+        {
+            Application.LoadLevel(2);
+        }
+    }
+}
