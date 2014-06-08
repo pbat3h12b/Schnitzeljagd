f41dad984bf08764dfba64a72aef9b9902ff10e1
Patrick Meyer <space@potential-terrorist.com>
2014-06-08 13:17:51 +0200

    More unittest refactoring

--- Datenbank/Cherrypy-Rest/unittests/apiTests.py
+++ Datenbank/Cherrypy-Rest/unittests/apiTests.py
@@ -9,6 +9,7 @@
 __author__ = "space"
 
 import unittest
+import hashlib
 import uuid
 import json
 import requests
@@ -30,32 +31,59 @@ config = {
 }
 
 class BaseFunctions(object):
+	def token(self, session):
+		username 				= session["username"]
+		session_secret          = session["session_secret"]
+		session_command_counter = session["command_counter"]
+		token = hashlib.md5((session_secret + str(session_command_counter)).encode('utf-8')).hexdigest()
+		session["command_counter"] += 1
+
+		return (token)	
+
+	def genericRestCall(self, command_url, payload, expected_error = None):
+		response  = requests.post(command_url, data=payload)
+		log.info(response.text)
+		response_json = json.loads(response.text)
+		
+		if (expected_error):
+			self.assertFalse(response_json["success"])
+			self.assertEqual(response_json["error"], expected_error)
+
+		else:
+			self.assertTrue(response_json["success"])
+		
+		return response_json
+
+
 	def login(self, payload, expected_error = None):
 		login_url = (config["api_url"] + "login")
-		response  = requests.post(login_url, data=payload)
-		log.info(response.text)
-		response_json = json.loads(response.text)
-		
-		if (expected_error):
-			self.assertFalse(response_json["success"])
-			self.assertEqual(response_json["error"], expected_error)
+		response_json = self.genericRestCall(login_url, payload, expected_error)
+		if not (response_json["success"]): return False
 
-		else:
-			self.assertTrue(response_json["success"])
-			self.assertIsInstance(response_json["session_secret"], unicode)
+		self.assertIsInstance(response_json["session_secret"], unicode)
+
+		session = { "username" : payload["username"],
+					"command_counter" : 0,
+					"session_secret":  response_json["session_secret"]}
+
+		return session
 
 	def register(self, payload, expected_error = None):
-		register_url=(config["api_url"] + "register")
-		response = requests.post(register_url, data=payload)
-		log.info(response.text)
-		response_json = json.loads(response.text)
-		
-		if (expected_error):
-			self.assertFalse(response_json["success"])
-			self.assertEqual(response_json["error"], expected_error)
+		register_url = (config["api_url"] + "register")
+		response_json = self.genericRestCall(register_url, payload, expected_error)
+		if not (response_json["success"]): return False
 
-		else:
-			self.assertTrue(response_json["success"])
+		return response_json	
+
+	def nop(self, session, expected_error = None):
+		nop_url=(config["api_url"] + "nop")
+
+		payload = {	'username': session["username"], 
+					'token': self.token(session) }
+
+		response_json = self.genericRestCall(nop_url, payload, expected_error)
+		if not (response_json["success"]): return False
+
 
 
 class RegisterTests(unittest.TestCase, BaseFunctions):
@@ -103,6 +131,21 @@ class LoginTests(unittest.TestCase, BaseFunctions):
 
 		self.register(payload)
 		self.login(payload)
+
+	def testforwardLoginTokenUsage(self):
+		payload = {	'username': config["existing_user"], 
+					'password': config["existing_users_password"] }
+
+		session = self.login(payload)
+		session = self.nop(session)
+
+	def testLoginWrongTokenUsage(self):
+		payload = {	'username': config["existing_user"], 
+					'password': config["existing_users_password"] }
+
+		session = self.login(payload)
+		session["session_secret"] = "Eris the younger"
+		session = self.nop(session, "Invalid Authentication Token.")
 
 
 if __name__ == '__main__':
