11e6faea91fba19c55b7ad52033c806fe695142e
Niclas Hüppmeier <niclas_hueppmeier@gmx.de>
2014-06-12 12:47:36 +0200

    änderungen und startup Funktionen

--- Unity3D_Projekte/Quantums_Quest_HauptSpiel/Assets/static/PlayerInformation.cs
+++ Unity3D_Projekte/Quantums_Quest_HauptSpiel/Assets/static/PlayerInformation.cs
@@ -7,6 +7,11 @@ public class PlayerInformation : MonoBehaviour {
     private static float longitude;
     private static float latitude;
     private static bool[] caches = {false,false,false,false,false,false};
+    private static string[] logBook = new string[5];
+    private static string[] gameNames = { "Lookpick", "Galaxy Invaders", "Wohnheim Spiel", "Angel Spiel", "Endkapmf Spiel" };
+    private static bool[] games = new bool[5];
+    private static int[] highscores = new int[5];
+    private static float timeSinceUpdate;
 
 
 	// Use this for initialization
@@ -19,12 +24,51 @@ public class PlayerInformation : MonoBehaviour {
 
         longitude = Input.location.lastData.longitude;
         latitude = Input.location.lastData.latitude;
+        updateGeoData();
 
 	}
 
-    void LogIn()
-    { 
-
+    void logIn(string name)
+    {
+        userName = name;
     }
+
+    void getUserData()
+    {
+        caches = GameObject.Find("GameController").GetComponent<beispiel_api>().getCaches(userName);
+        games = caches;
+        highscores = GameObject.Find("GameController").GetComponent<beispiel_api>().getHighscors(userName);
+    }
+
+    void updateGeoData()
+    {
+        if (timeSinceUpdate > 60)
+        {
+            GameObject.Find("GameController").GetComponent<beispiel_api>().updateGeoData(longitude, latitude);
+        }
+    }
+
+    void updateCach(int ID)
+    {
+        caches[ID - 1] = true;
+        GameObject.Find("GameController").GetComponent<beispiel_api>().updateCach(ID);
+    }
+
+    void newScore(int SpielID, int Score)
+    {
+        if (highscores[SpielID - 1] < Score)
+        {
+            highscores[SpielID - 1] = Score;
+        }
+
+        GameObject.Find("GameController").GetComponent<beispiel_api>().newScore(SpielID, Score);
+    }
+
+    void newlogBook( int CachID, string Text )
+    {
+        logBook[CachID - 1] = Text;
+        GameObject.Find("GameController").GetComponent<beispiel_api>().newLogBook(Text, CachID);
+    }
+
 }
 
