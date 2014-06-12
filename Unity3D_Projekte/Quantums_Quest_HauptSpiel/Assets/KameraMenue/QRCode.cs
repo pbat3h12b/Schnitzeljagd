623b5d6a2d9afb0d2c6d060c33aa40f94f177d37
Niclas Hüppmeier <niclas_hueppmeier@gmx.de>
2014-06-12 13:30:41 +0200

    qr-Code & Log Buch GUI von Martin Luong eingefügt

---
+++ Unity3D_Projekte/Quantums_Quest_HauptSpiel/Assets/KameraMenue/QRCode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QRCode : MonoBehaviour {
+
+	string qrcode = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.Box (new Rect (Screen.width / 13, Screen.height / 13, Screen.width * 0.85f, Screen.height / 2 + 100), "");
+		GUI.Label (new Rect (Screen.width / 11, Screen.height / 10, 200, 100), "QR-Code Scanner");
+		qrcode = GUI.TextField (new Rect (Screen.width / 11, Screen.height / 5, Screen.width / 2, 25), qrcode);
+		GUI.Label (new Rect (Screen.width / 11, Screen.height / 4, 200, 50), "Bitte Code eingeben");
+		GUI.Button (new Rect (Screen.width / 2 - 130, Screen.height / 3, 100, 100), "Zurück");
+		GUI.Button (new Rect (Screen.width / 2, Screen.height / 3, 100, 100), "Weiter");
+	}
+}
---
+++ Unity3D_Projekte/Quantums_Quest_HauptSpiel/Assets/LogBuchEintrag/LogbuchEintrag.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogbuchEintrag : MonoBehaviour {
+
+	string lgeintrag = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.Box (new Rect (Screen.width / 13, Screen.height / 13, Screen.width * 0.85f, Screen.height / 2 + 100), "");
+		lgeintrag = GUI.TextField (new Rect (Screen.width / 11, Screen.height / 9, Screen.width / 2, Screen.width / 3), lgeintrag);
+		GUI.Label (new Rect (Screen.width / 2 + 200, Screen.height / 9, 200, 200), "Hier können Sie eine Nachricht hinterlassen");
+		GUI.Button (new Rect (Screen.width / 2 + 250, Screen.height / 3, 100, 100), "Weiter");
+	}
+}
