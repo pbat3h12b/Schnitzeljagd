8ac32c65e513f49af680ec093fcdf9da8225c745
Oliver Noll <langustee@gmail.com>
2014-07-02 21:55:07 +0200

    Fix: Geoposition, Neues Unity-Projekt

---
+++ Unity3D_Projekte/Quantums_Quest/Assets/MainMenue/GameControll_MainMenue.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControll_MainMenue : MonoBehaviour {
+    [HideInInspector]
+    private int screenWidth;
+    private int screenHeight;
+    private double scaleWidth;
+    private double scaleHeight;
+    // Use this for initialization
+    void Start()
+    {
+        screenHeight = Screen.width;
+        screenWidth = Screen.height;
+
+        scaleWidth = screenWidth / 100;
+        scaleHeight = screenHeight / 100;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect((float)((screenWidth / 2) - ((50 * scaleWidth) / 2)), (float)((screenHeight / 2) + (5 * scaleHeight)), (float)(50 * scaleWidth), (float)(5 * scaleHeight)),"Karte Anzeigen"))
+        {
+            Application.LoadLevel(4);
+        }
+
+        if (GUI.Button(new Rect((float)((screenWidth / 2) - ((50 * scaleWidth) / 2)), (float)((screenHeight / 2) - (5 * scaleHeight)), (float)(50 * scaleWidth), (float)(5 * scaleHeight)), "Spiele Anzeigen"))
+        {
+            Application.LoadLevel(3);
+        }
+    }
+}
