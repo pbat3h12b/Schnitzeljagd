c892a518d59acf8976fe5708c39fbefcc0e42ccb
Markus Baars <mbaars@web.de>
2014-08-27 23:46:41 +0200

    Neue Version des Bosskampfes, KW35 Wochenbericht

---
+++ Unity3D_Projekte/Bosskampf-Unity-Projekt/Assets/Scripts/AccelScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate(Input.acceleration.x * Time.deltaTime , Input.acceleration.y * Time.deltaTime,0 );
+	}
+}
