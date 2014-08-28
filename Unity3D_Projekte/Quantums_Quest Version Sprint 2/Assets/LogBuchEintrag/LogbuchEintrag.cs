f99c6d758fb95b28dba2413d9bf504f8ce5d54d7
Niclas Hüppmeier <niclas_hueppmeier@gmx.de>
2014-08-28 11:04:23 +0200

    .

--- Unity3D_Projekte/Quantums_Quest Version Sprint 2/Assets/LogBuchEintrag/LogbuchEintrag.cs
+++ Unity3D_Projekte/Quantums_Quest Version Sprint 2/Assets/LogBuchEintrag/LogbuchEintrag.cs
@@ -28,9 +28,9 @@ public class LogbuchEintrag : MonoBehaviour {
         GUI.Box(gameController.GetComponent<GUI_Scale>().GetRelativeRect(new Rect(10, 10, 80, 80)), "");
         lgeintrag = GUI.TextField(gameController.GetComponent<GUI_Scale>().GetRelativeRect(new Rect(15, 15, 50, 70)), lgeintrag);
         GUI.Label(gameController.GetComponent<GUI_Scale>().GetRelativeRect(new Rect(70, 15, 10, 10)), "Hier können Sie eine Nachricht hinterlassen");
-        if (GUI.Button(gameController.GetComponent<GUI_Scale>().GetRelativeRect(new Rect(67, 30, 20, 10)), "", styleWeiter))
+        if (GUI.Button(gameController.GetComponent<GUI_Scale>().GetRelativeRect(new Rect(67, 30, 20, 10)), "", styleWeiter) && lgeintrag != "")
         {
-
+            Response temp = gameController.GetComponent<RESTCommunication>()
         }
 	}
 }
