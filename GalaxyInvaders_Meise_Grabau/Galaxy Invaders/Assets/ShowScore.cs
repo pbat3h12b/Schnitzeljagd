37a44be1aab068dfe36c664a59caf979f4ec8658
Anonym <xenomorph@localhost.localdomain>
2014-09-09 19:34:15 +0200

    abgabe Galaxy Invaders

---
+++ GalaxyInvaders_Meise_Grabau/Galaxy Invaders/Assets/ShowScore.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowScore : MonoBehaviour {
+	//Klasse zur Lebensverwaltung von Objekten
+	//Erstellt von Fabian Meise
+	//!!!Klasse unter Bearbeitung"
+	
+	#region Attribute
+	//erreichte Punkte
+	int score=0;
+	#endregion
+	
+	
+	#region Methoden
+	// Use this for initialization
+	void Start () {
+		OnGUI ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//Addierung der Punkte
+	public void ApplyScore(int points)
+	{
+		score += points;
+	}
+
+	void OnGUI()
+	{
+
+		//FONT = GUI.skin.font = MyFont;
+		//Verändern der Schriftgröße um die Score lesbar zu machen
+		GUIStyle fontStyle = new GUIStyle();
+		fontStyle.fontSize = 25;
+		GUI.color = Color.red;
+		//Textfeld dass die bereits erreichte Punktzahl des Spielers anzeigt 
+		GUI.Label(new Rect(0+Screen.width/50,0+Screen.height/50, 100,100),"" + score);
+	}
+	#endregion
+}
---
+++ GalaxyInvaders_Meise_Grabau/old/Galaxy Invaders/Assets/Score.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	// Use this for initialization
+
+	// Übergeben wird der Wert Score, momentan nur Hilfsvariable
+	int score = 0;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI()
+	{
+
+		//FONT = GUI.skin.font = MyFont;
+		//Verändern der Schriftgröße um die Score lesbar zu machen
+		GUIStyle fontStyle = new GUIStyle();
+		fontStyle.fontSize = 25;
+		//Textfeld dass die bereits erreichte Punktzahl des Spielers anzeigt 
+		GUI.Label(new Rect(Screen.width/1.2f,Screen.height/10, 100,100), "="+score, fontStyle);
+	}
+}
