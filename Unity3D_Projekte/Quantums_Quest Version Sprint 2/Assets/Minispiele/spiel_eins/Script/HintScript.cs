d1d36133c784755755a55fc97e24bfa8536168ff
Niclas Hüppmeier <niclas_hueppmeier@gmx.de>
2014-09-09 19:41:18 +0200

    einfügen der Spiele

---
+++ Unity3D_Projekte/Quantums_Quest Version Sprint 2/Assets/Minispiele/spiel_eins/Script/HintScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HintScript : MonoBehaviour {
+
+	private GameObject gcScript;
+
+	//Benötigte Variabeln für Hindernisse
+	bool isBomb = false;
+	bool isShear = false;
+
+	//Ziel Erreicht
+	bool isGoal = false;
+
+	// Use this for initialization
+	void Start () {
+		gcScript = GameObject.FindGameObjectWithTag("GameController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.name == "Rock(Clone)" && isBomb == false || other.name == "Hedge(Clone)" && isShear == false) {
+			gcScript.SendMessage ("ChangeTrigger");
+		}
+		if (other.name == "Rock(Clone)" && isBomb == true || other.name == "Hedge(Clone)" && isShear == true) {
+			GameObject.Destroy (other.gameObject);
+		}
+		if (other.name == "Bomb(Clone)") {
+			isBomb = true;
+			GameObject.Destroy(other.gameObject);
+		}
+		if (other.name == "Shear(Clone)") {
+			isShear = true;
+			GameObject.Destroy (other.gameObject);
+		}
+		if(other.name == "Waypoint11"){
+			isGoal = true;
+		}
+		
+	}
+	
+	void OnGUI(){
+		if (isGoal == true) {
+			GUI.Label(new Rect(Screen.width /2,Screen.height / 2, Screen.width * 2, Screen.height * 2), "Gewonnen");
+			isGoal = false;
+			isBomb = false;
+			isShear = false;
+			//gcScript.SendMessage("Restart");
+		}
+	}
+}
---
+++ Unity3D_Projekte/Quantums_Quest Version Sprint 2/Assets/Minispiele/spiel_vier/EnemyControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyControl : MonoBehaviour {
+
+	public GameObject parent;
+	float movementTimer = 1.0f;
+	float spawnTimer = 1.0f;
+	int speedY = 1;
+	Vector3 position;
+	float bottom = Screen.height - (Screen.height - 1);
+	float top = Screen.height;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	//Ursprünglich waren die auskommentierten Zeilen dazu da ein stockendes Bewegen der Gegner
+	//zu verursachen - allerdings hat dies dazu geführt das Projektile nicht
+	//sofort verschwanden wenn sie auf einen Gegner trafen
+		//movementTimer += Time.deltaTime;
+		spawnTimer += Time.deltaTime;
+		//position = parent.transform.position;
+		//if (movementTimer > 1) 
+		//{
+		//	position.y -= speedY;
+		//	parent.transform.position = position;
+		//	movementTimer = 0;
+		//}
+
+		//Aus dem oben genannten Grund wurde die Bewegung der Gegner fließend gemacht
+		position.y-=Time.deltaTime*speedY;
+		parent.transform.position = position;
+		//Timer um Zeit in Sekunden zwischen den Erstellungen der Wellen zu bestimmen
+		if(spawnTimer > 18)
+		{
+			SpawnNewWave();
+			spawnTimer = 0;
+		}
+	}
+
+	//Erstellt die neue Welle von Gegnern indem es die Erstellungsfunktion initlevel()
+	//der Methode EnemySpawn aufruft.
+	void SpawnNewWave()
+	{
+		parent.GetComponent<EnemySpawn>().initLevel();
+	}
+
+}
