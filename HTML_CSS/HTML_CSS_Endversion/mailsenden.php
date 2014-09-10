3e80c3181164c7222a8dfc5bfce4e27f9cd0f51c
Ebbers <l.ebbers@gmx.net>
2014-09-10 08:28:02 +0200

    Kleine Überarbeitung

--- HTML_CSS/HTML_CSS_Endversion/mailsenden.php
+++ HTML_CSS/HTML_CSS_Endversion/mailsenden.php
@@ -1,8 +1,8 @@
-<?PHP
+<?PHP //Erstellt von Lukas Ebbers
 
 $text = "Kontaktaufname von: ".$_POST[name]."\n Seine Nachricht lautet:".$_POST[nachricht]."\n\n Antworten können Sie hier:".$_POST[email];
 
-mail("a@gmx.de","Email per Formular",$text);
+mail("dozkeh@pb.bib.de","Email per Formular",$text);
 
-header('Location: http://unkolunix.de/vorprojekt/contact.php'); exit; 
+header('Location: http://btcwash.de/vorprojekt/contact.php'); exit; 
 ?>
