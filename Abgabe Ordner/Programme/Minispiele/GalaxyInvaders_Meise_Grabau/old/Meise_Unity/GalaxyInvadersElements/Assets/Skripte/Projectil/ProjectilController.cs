db8b69aaeed515ff08d227cd9d2b2baf11cb3493
Niclas Hüppmeier <niclas_hueppmeier@gmx.de>
2014-09-10 14:47:23 +0200

    ..

---
+++ Abgabe Ordner/Programme/Minispiele/GalaxyInvaders_Meise_Grabau/old/Meise_Unity/GalaxyInvadersElements/Assets/Skripte/Projectil/ProjectilController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectilController : MonoBehaviour {
+
+	//Klasse zur Lebensverwaltung von Objekten
+	//Erstellt von Fabian Meise am 23.8.2014
+	//Zuletzt bearbeitet am 28.8.2014
+
+	#region Attribute
+	//Bewegungsgeschwindigkeit
+	public float speed=0.04f;
+	//Um Sicher Zustellen, dass das Projectil außerhalb des
+	//Bildschirms verschwindet wird es an den Rändern "zerstört"
+	//oberer Rand des Bildschirms
+	public float borderTop=5;
+	//unterer Rand des Bildschirms
+	public float borderbottom=-5;
+	#endregion
+
+	#region Methoden
+	// Update is called once per frame
+	void Update () {
+		//Vorwärtsbewegung des Projectils
+		transform.position = new Vector3 (transform.position.x,
+		                                  transform.position.y + speed);
+		Checkposition();
+	}
+
+	//Positionkontrollieren
+	void Checkposition()
+	{
+		if (transform.position.y > borderTop)
+		{
+			Destroythis();
+		}
+		else if (transform.position.y < borderbottom)
+		{
+			Destroythis();
+		}
+	}
+
+	//Gameobject wird zerstört
+	void Destroythis()
+	{
+		GameObject.Destroy (this.gameObject);
+	}
+	#endregion
+}
---
+++ Abgabe Ordner/Programme/Minispiele/GalaxyInvaders_Meise_Grabau/old/Meise_Unity/GalaxyInvadersElements/Assets/Skripte/ShowScore.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowScore : MonoBehaviour {
+
+	//Klasse zur Lebensverwaltung von Objekten
+	//Erstellt von Fabian Meise
+	//!!!Klasse unter Bearbeitung"
+
+	#region Attribute
+	//erreichte Punkte
+	int score=0;
+	#endregion
+	
+
+	#region Methoden
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Addierung der Punkte
+	public void ApplyScore(int points)
+	{
+		score += points;
+	}
+	#endregion
+}
